899a795930fad7652c5de283beac9b21ff797adf
melvynrouault <melvyn.rouault@gmail.com>
2021-05-08 17:56:22 +0200

    adapt port for device

--- src/config/typeormconfig.ts
+++ src/config/typeormconfig.ts
@@ -3,7 +3,8 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 export const typeormConfig: TypeOrmModuleOptions = {​​​​
     ​type: 'postgres',
     host: 'localhost',
-    port: 5433,
+    // port: 5433, // port pcfixe
+    port: 5432, //port pcportable
     username: 'postgres',
     password: 'root',
     database: 'albumcollection',
